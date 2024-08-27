c5c2ff8a94ccfa6f4d6b4f62f913ca83d49f1a93
Sarwar Miral <sarwar.miral@bjitgroup.com>
2024-08-27 12:16:06 +0600

    Initial commit

---
+++ src/main/java/com/bjit/ci/DemoProjectCiApplication.java
@@ -0,0 +1,13 @@
+package com.bjit.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjit/ci/DemoProjectCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjit.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
